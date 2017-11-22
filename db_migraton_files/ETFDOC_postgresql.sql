6a0d1341614a99d78806d179b929a4ec38b4d5f5
acimovicAlek <acimovic.alek@gmail.com>
2017-11-22 18:08:29 +0100

    Created folders

---
+++ db_migraton_files/ETFDOC_postgresql.sql
@@ -0,0 +1,12 @@
+﻿-- Database: "ETFDOC"
+
+-- DROP DATABASE "ETFDOC";
+
+CREATE DATABASE "ETFDOC"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+
